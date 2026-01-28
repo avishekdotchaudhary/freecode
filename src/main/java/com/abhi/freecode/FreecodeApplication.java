99112cd474836ffee70e3afc37ba801da2236658
avishekdotchaudhary <avishekdotchaudhary@gmail.com>
2026-01-28 22:44:51 +0545

    initial commit

---
+++ src/main/java/com/abhi/freecode/FreecodeApplication.java
@@ -0,0 +1,13 @@
+package com.abhi.freecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreecodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreecodeApplication.class, args);
+    }
+
+}
