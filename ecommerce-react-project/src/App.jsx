50d7f9cb6bbed89ec6d90e2ab85b3d0b12bd90e8
avishekdotchaudhary <avishekdotchaudhary@gmail.com>
2026-03-11 20:37:31 +0545

    Tracking page added

--- ecommerce-react-project/src/App.jsx
+++ ecommerce-react-project/src/App.jsx
@@ -3,6 +3,7 @@ import { HomePage } from "./pages/HomePage.jsx";
 import { Routes, Route } from "react-router";
 import { CheckoutPage } from "./pages/CheckoutPage.jsx";
 import { OrdersPage } from "./pages/OrdersPage.jsx";
+import { TrackingPage } from "./pages/TrackingPage.jsx";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="checkout" element={<CheckoutPage />} />
         <Route path="orders" element={<OrdersPage />} />
+        <Route path="tracking" element={<TrackingPage />} />
       </Routes>
     </>
   );
